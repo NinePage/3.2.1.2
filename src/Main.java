b95dbb9bb7563356481f334a6acbd57de722ad8e
NinePage <ninepageart@gmail.com>
2021-07-30 16:32:58 +0400

    Start

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int replenishment = 1100;
+        int bonus = 0;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        }
+        int total = balance + replenishment + bonus;
+        System.out.println("Total: " + total);
+    }
+}
